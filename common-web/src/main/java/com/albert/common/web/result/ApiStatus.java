7b2c3d7582bcf8131bb8b4bf7893fbab4eb1a22a
Albert <monkeybrother23@gmail.com>
2022-04-20 15:57:52 +0800

    message

--- common-web/src/main/java/com/albert/common/web/result/ApiStatus.java
+++ common-web/src/main/java/com/albert/common/web/result/ApiStatus.java
@@ -4,11 +4,18 @@ public enum ApiStatus implements ApiMessage {
     SUCCESS("00010", "success"),
     FAIL("00011", "fail"),
     ERROR("00012", "error"),
-
     FORBIDDEN("00020", "forbidden"),
     VALIDATION("00030", "validation"),
+    NULLPOINTER_EXCEPTION("00040", "NullPointerException"),
 
-    NULLPOINTER_EXCEPTION("00040", "NullPointerException");
+    SAVE_SUCCESS("00050", "Save success"),
+    SAVE_FAIL("00051", "Save fail"),
+    DELETE_SUCCESS("00052", "Delete success"),
+    DELETE_FAIL("00053", "Delete fail"),
+    UPDATE_SUCCESS("00054", "Update success"),
+    UPDATE_FAIL("00055", "Update fail"),
+    QUERY_SUCCESS("00056", "Query success"),
+    QUERY_FAIL("00057", "Query fail");
     private final String code;
     private final String msg;
 
